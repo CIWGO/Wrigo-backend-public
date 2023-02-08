bc351eeb6031278c53d59b643dafb08b2c9ad62e
Shuchen Wu <68723186+ShuchenWuu@users.noreply.github.com>
2023-02-08 23:27:11 +1030

    Add the user login feature (#17)
    Read user information (ticket 30 feature merged in this commit). Find if there is such a user in the database. If there is such a user, check if the password matches the user's password. Return with the corresponding status code and messages.

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import { createUser, findUser, updateUser, deleteUser } from "../controllers/userService";
-
+import {
+  createUser,
+  findUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/userService";
+import { login } from "../controllers/userLogin";
 const userRouter = Router();
 
 userRouter.post("/signup", createUser);
 userRouter.get("/findUser", findUser);
 userRouter.put("/updateUser", updateUser);
 userRouter.delete("deleteUser", deleteUser);
+
+userRouter.post("/login", login);
 
 export { userRouter };
