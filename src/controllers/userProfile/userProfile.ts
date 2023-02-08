a2d073b857c39aec46c701c634786fce3a632ff1
Zihan Zhou <56948832+ZihanZhou21@users.noreply.github.com>
2023-02-08 23:52:42 +1100

    Ciwgo 12 collect other user info (#11)
    * createUserProfile and updateUserProfile
    
    * modified:   src/routes/user.ts
    
    * fix problems
    
    * delete commends
    
    * solve path error

---
+++ src/controllers/userProfile/userProfile.ts
@@ -0,0 +1,42 @@
+import UserModel from "../../models/users";
+import { Response, Request } from "express";
+const showUserProfile = async (req: Request, res: Response) => {
+  const { username } = req.params;
+  const user = await UserModel.findOne({ username }).exec();
+  if (!user) return res.status(404).json({error:"user not found"});
+  res.json(user);
+};
+
+const createUserProfile = async (req: Request, res: Response) => {
+  const { username } = req.params;
+  const {email, country, gender, birthday} = req.body;
+  const user = await UserModel.findOne({ username }, {
+    $push: {
+      user: {email, country, gender, birthday},
+    },
+  },
+  { new: true },).exec();
+  if (!user) return res.sendStatus(404);
+  res.send(user);
+};
+
+const updateUserProfile = async (req: Request, res: Response) => { 
+  const { username } = req.params;
+  const { email, country, gender, birthday } = req.body;
+  const user = await UserModel.findOneAndUpdate(
+    { username },
+    {
+      $set: {
+        "email": email,
+        "country": country,
+        "gender": gender,
+        "birth":birthday
+      },
+    },
+    { new: true },
+  ).exec();
+  res.send(user);
+
+};
+
+export {showUserProfile,createUserProfile,updateUserProfile};
