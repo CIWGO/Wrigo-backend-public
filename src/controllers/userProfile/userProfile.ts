992b7b43cb701c8bd5259800702a2ea3697c6def
Henry <zihanzhou1021@gmail.com>
2023-02-10 12:15:50 +1100

    merge main branch

--- src/controllers/userProfile/userProfile.ts
+++ src/controllers/userProfile/userProfile.ts
@@ -4,24 +4,28 @@ import { Response, Request } from "express";
 const showUserProfile = async (req: Request, res: Response) => {
   const { username } = req.body;
   const user = await UserModel.findOne({ username }).exec();
+
   if (!user) return res.status(404).json({error:"user not found"});
-  res.status(201).json(user);
-};
+  res.status(200).json(user);
+}; 
 
 const createUserProfile = async (req: Request, res: Response) => {
-  const { username, country,birth, gender, study_field } = req.body;
+  const { username, country, birth, gender, study_field } = req.body;
+  
   const user = await UserModel.findOneAndUpdate({ username }, {
     $set: {
       country,gender,birth,study_field
     },
   },
-  {upsert: true, new: true},).exec();
+    { upsert: true, new: true },).exec();
+  
   if (!user) return res.sendStatus(404);
-  res.send(user);
+  res.status(201).send(user);
 };
 
 const updateUserProfile = async (req: Request, res: Response) => { 
-  const { username, country,birth, gender, study_field } = req.body;
+  const { username, country, birth, gender, study_field } = req.body;
+  
   const user = await UserModel.findOneAndUpdate(
     { username },
     {
@@ -31,9 +35,9 @@ const updateUserProfile = async (req: Request, res: Response) => {
     },
     { upsert: true, new: true },
   ).exec();
-  if (!user) return res.sendStatus(404);
-  res.send(user);
 
+  if (!user) return res.sendStatus(404);
+  res.status(200).send(user);                                                                                                                                             
 };
 
 export {showUserProfile,createUserProfile,updateUserProfile};
