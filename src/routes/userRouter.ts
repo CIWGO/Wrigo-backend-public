66a4232b9c0a391e5f4508697d64014037ff25ea
Athena Wu <wushuchen113@gmail.com>
2023-02-11 13:45:03 +1030

    Change password with email notification to user

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -6,6 +6,7 @@ import {
   deleteUser,
 } from "../controllers/userService";
 import { login } from "../controllers/userLogin";
+import { changePassword } from "../controllers/changePassword";
 const userRouter = Router();
 
 userRouter.post("/signup", createUser);
@@ -14,5 +15,6 @@ userRouter.put("/updateUser", updateUser);
 userRouter.delete("deleteUser", deleteUser);
 
 userRouter.post("/login", login);
+userRouter.patch("/changePassword", changePassword);
 
 export { userRouter };
