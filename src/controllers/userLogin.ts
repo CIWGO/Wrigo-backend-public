f1b08da3f70d5152bf6f9845d96aefe474c291f4
Athena Wu <wushuchen113@gmail.com>
2023-02-14 14:49:45 +1030

    Modifying  user session functions

--- src/controllers/userLogin.ts
+++ src/controllers/userLogin.ts
@@ -1,20 +1,20 @@
 import UserModel from "../models/users";
 import { Request, Response } from "express";
-import { createUserSession } from "./userSession";
+import { generateToken } from "../utils/jwt";
 
 const login = async (req: Request, res: Response) => {
-	try {
-		const { username, password } = req.body;
-		const user = await UserModel.findOne({ username }).exec();
+  try {
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username }).exec();
 
-		if (!user) {
-			return res.status(404).json({ error: "User not found" });
-		}
-		if (user.password !== password) {
-			return res.status(404).json({ error: "Incorrect password" });
-		}
-    
-    const token = createUserSession(user);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    if (user.password !== password) {
+      return res.status(404).json({ error: "Incorrect password" });
+    }
+
+    const token = generateToken(user.username);
     res.status(200).json({ message: "Login successful", token });
   } catch (error) {
     res.status(500).json({ error: error.message || "Server error" });
--- src/controllers/userSession.ts
+++ src/controllers/userSession.ts
@@ -1,14 +1,35 @@
-import jwt from "jsonwebtoken";
+// import { Request, Response } from "express";
+// import { validateToken } from "../utils/jwt"
 
-const createUserSession = (user) => {
-  const payload = {
-    username: user.username,
-  };
+// const createUserSession = (user) => {
+//   const payload = {
+//     username: user.username,
+//   };
 
-  const secret = process.env.JWT_SECRET;
-  const options = { expiresIn: "1h" };
+//   const secret = process.env.JWT_SECRET;
+//   const options = { expiresIn: "1h" };
 
-  return jwt.sign(payload, secret, options);
-};
+//   return jwt.sign(payload, secret, options);
+// };
 
-export { createUserSession };
+// const validateToken = (token) => {
+
+// }
+
+//valid authorization format: Bearer {token}
+// const tokenGuard = (req: Request, res: Response){
+//   const authorization = req.header('Authorization');
+
+//   if (!authorization) {
+//     return res.status(401).json({ error: "missing the authorization header" });
+//   }
+
+//   const tokenArray = authorization.split(' ');
+//   if (tokenArray.length != 2 || tokenArray[0] != 'Bearer') {
+//     return res.status(401).json({ error: "invalid authorization header format" });
+//   }
+//   const payload = validateToken(tokkenArray[1]);
+
+// }
+
+// export { createUserSession };
