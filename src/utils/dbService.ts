2cef180ee59a6044a49f38ea8af7c8b635676a99
leoh <leoyh.97@gmail.com>
2023-02-14 19:32:45 +0800

    fix main

--- src/utils/dbService.ts
+++ src/utils/dbService.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import config from "../config";
+import config from "../../config";
 
 const connectToDB = () => {
 	const connectionString = config.CONNECTION_STRING;
