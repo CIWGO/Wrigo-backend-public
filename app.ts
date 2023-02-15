cdf4bdadaed4d1378d0b2b6a0a6db102740c2665
Reneezzzzz <112042893+Reneezzzzz@users.noreply.github.com>
2023-02-16 10:49:03 +1100

    frontend essayInputPage basicNavigatiion and router, backend Cors enabled

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from "express";
+import cors from "cors";
 
 require("express-async-errors");
 
 const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
 
 export default app;
---
+++ ciwgo-test-app/src/NavigationPage/NavigationPage.jsx
@@ -0,0 +1,16 @@
+import "./NavPage.css";
+function NavigationPage() {
+  return (
+    <div className="box">
+      <header>
+        <p>essay input page implemented, please navigate to /evaluate to use</p>
+
+        <a href="/evaluate" className="link">
+          essay evaluate page
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default NavigationPage;
