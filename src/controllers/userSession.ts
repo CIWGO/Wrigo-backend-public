d0dd9e960c12f4d0babbf014916a6114ba2ce717
HAIXIN ZHANG <haixin.zhang@go1.com>
2023-02-16 14:10:02 +0800

    format code

--- src/controllers/userSession.ts
+++ src/controllers/userSession.ts
@@ -2,31 +2,31 @@ import { Request, Response } from "express";
 import { validateToken } from "../utils/jwt";
 
 interface AuthRequest extends Request {
-  user?: any;
+  user?: unknown;
 }
 
 // call this middleware for functions that need to be protected by user login token
 const tokenGuard = (req: AuthRequest, res: Response, next) => {
-  const authorization = req.header("Authorization");
-  if (!authorization) {
-    return res.status(401).json({ error: "missing the authorization header" });
-  }
+	const authorization = req.header("Authorization");
+	if (!authorization) {
+		return res.status(401).json({ error: "missing the authorization header" });
+	}
 
-  // valid authorization format: Bearer {token}
-  const tokenArray = authorization.split(" ");
-  if (tokenArray.length != 2 || tokenArray[0] != "Bearer") {
-    return res
-      .status(401)
-      .json({ error: "invalid authorization header format" });
-  }
+	// valid authorization format: Bearer {token}
+	const tokenArray = authorization.split(" ");
+	if (tokenArray.length != 2 || tokenArray[0] != "Bearer") {
+		return res
+			.status(401)
+			.json({ error: "invalid authorization header format" });
+	}
 
-  try {
-    const payload = validateToken(tokenArray[1]);
-    req.user = payload;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
+	try {
+		const payload = validateToken(tokenArray[1]);
+		req.user = payload;
+		next();
+	} catch (err) {
+		return res.status(401).json({ message: "Invalid token" });
+	}
 };
 
 export { tokenGuard };
