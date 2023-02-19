ed80237a3decffaa4fdf03fdb2bf4d31c9a52ba9
EricSherlockLi <helloleolee@hotmail.com>
2023-02-19 17:09:42 +0800

    modified changePassword to one func

--- src/controllers/user/changePassword.ts
+++ src/controllers/user/changePassword.ts
@@ -1,14 +1,17 @@
-import UserModel from "../../models/user/userAccount";
+import {userAccount as UserModel} from "../../models/index";
 import { Request, Response } from "express";
 import { sendEmail } from "../../utils/emailNotification";
+
+// remove findUserEmail as no need to verify exsitence of email
 
 const changePassword = async (req: Request, res: Response) => {
 	try {
 		const { username, password } = req.body;
-		const user = await UserModel.findOne({ username }).exec();
-		const email = await findUserEmail(username);
 
+		const user = await UserModel.findOne({ username }).exec();
 		if (!user) return res.status(404).send("User not found");
+
+		const email = user.email;
 		const result = await UserModel.updateOne(
 			{ username },
 			{ $set: { password: password } }
@@ -16,27 +19,16 @@ const changePassword = async (req: Request, res: Response) => {
 
 		// modifiedCount containing the number of modified documents
 		if (result.modifiedCount === 0) {
-			return res.status(404).json({ error: "Error in changing password" });
-		} else if (result.modifiedCount === 1) {
+			return res.status(404).json({ error: "Error in changing password, Password not modified." });
+		} else {
 			const msg = `Hi ${username},\n\nYou recently requested to reset the password for your CIWGO account. `;
-
 			sendEmail("ciwgo-dev@hotmail.com", email, "Password Changed", msg);
 
 			return res.status(200).json({ message: "Password changed successfully" });
 		}
 	} catch (error) {
+		console.log(error);
 		res.status(500).send(error.message || "Error changing password2");
-	}
-};
-
-const findUserEmail = async (username: string): Promise<string> => {
-	// eslint-disable-next-line no-useless-catch
-	try {
-		const user = await UserModel.findOne({ username }, "email").exec();
-		if (!user) throw new Error("User not found");
-		return user.email;
-	} catch (error) {
-		throw error;
 	}
 };
 
