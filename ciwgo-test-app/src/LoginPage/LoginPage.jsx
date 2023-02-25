0eb9bacbcbee39b089289b10c27f2c3d2027988a
Zihan <zihanzhou1021@gmail.com>
2023-02-25 23:53:01 +1100

    "fix bug"

--- ciwgo-test-app/src/LoginPage/LoginPage.jsx
+++ ciwgo-test-app/src/LoginPage/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
