48667fc6f4e62079b14361ce58d973f89fbf06fa
leo <leoyh.97@gmail.com>
2023-03-09 15:33:09 +0800

    replaced old email notification with AWS SES

--- src/utils/ses_sendEmail.ts
+++ src/utils/ses_sendEmail.ts
@@ -2,41 +2,44 @@
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 import { fromIni } from "@aws-sdk/credential-provider-ini";
 
-const client = new SESClient({ region: "ap-southeast-2", credentials: fromIni({
-	profile: "default",
-	filepath: "../../.aws/credentials"
-})});
+const sendEmail = async (toAddresses: string[], subject: string, bodyText: string) => {
 
-const sendEmail = async () => {
-	const params = {
-		Destination: {
-			ToAddresses: ["leoyh.97@gmail.com"],
-		},
-		Message: {
-			Body: {
-				Html: {
-					Data: "<p>Hello World!</p>",
-				},
-				Text: {
-					Data: "Hello World!",
-				},
-			},
-			Subject: {
-				Data: "Test Email",
-			},
-		},
-		Source: "notification@wrigo.com.au",
-	};
+    const credentials = fromIni({
+        profile: "default",
+        filepath: ".aws/credentials", 
+      });
 
-	try {
-		const command = new SendEmailCommand(params);
-		const result = await client.send(command);
-		console.log(result);
-	} catch (error) {
-		console.error(error);
-	}
+    const client = new SESClient({ 
+        region: "ap-southeast-2", 
+        credentials
+    });
+  
+    const params = {
+      Destination: {
+        ToAddresses: toAddresses,
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: bodyText,
+          },
+        },
+        Subject: {
+          Data: subject,
+        },
+      },
+      Source: "wrigo-account-service@wrigo.com.au",
+    };
+  
+    try {
+      const command = new SendEmailCommand(params);
+      const result = await client.send(command);
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
 };
 
-sendEmail();
+export {sendEmail};
 
   
