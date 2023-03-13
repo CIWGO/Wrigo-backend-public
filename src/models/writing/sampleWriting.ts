541110ebea673d8f071ccd34a943cdb65cc663bd
AlbertDontCry <yuzhiqiang3014@gmail.com>
2023-03-13 17:04:49 +1000

    CIWGO-134: Change sample writing model

--- src/models/writing/sampleWriting.ts
+++ src/models/writing/sampleWriting.ts
@@ -5,7 +5,15 @@ export interface SampleWriting {
 	topic_id: string;
 	sampleWriting_id: string;
 	sampleWriting_content: string;
-	sampleWriting_feedback: string;
+	sampleFeedback_TR: string;
+	sampleFeedback_CC: string;
+	sampleFeedback_LR: string;
+	sampleFeedback_GRA: string;
+	sampleFeedback_overall: string;
+	sampleScore_TR: number;
+	sampleScore_CC: number;
+	sampleScore_LR: number;
+	sampleScore_GRA: number;
 }
 
 const schema = new Schema<SampleWriting>(
@@ -22,8 +30,41 @@ const schema = new Schema<SampleWriting>(
 		sampleWriting_content: {
 			type: String,
 		},
-		sampleWriting_feedback: {
+		sampleFeedback_TR: {
 			type: String,
+			required: true
+		},
+		sampleFeedback_CC: {
+			type: String,
+			required: true
+		},
+		sampleFeedback_LR: {
+			type: String,
+			required: true
+		},
+		sampleFeedback_GRA: {
+			type: String,
+			required: true
+		},
+		sampleFeedback_overall: {
+			type: String,
+			required: true
+		},
+		sampleScore_TR: {
+			type: Number,
+			required: true
+		},
+		sampleScore_CC: {
+			type: Number,
+			required: true
+		},
+		sampleScore_LR: {
+			type: Number,
+			required: true
+		},
+		sampleScore_GRA: {
+			type: Number,
+			required: true
 		},
 	},
 	{ collection: "sample_writing" }
