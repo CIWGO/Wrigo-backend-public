a33c420b4aecbb7cfcb191521e8071bd2c8d19c1
leo <leoyh.97@gmail.com>
2023-03-13 20:49:06 +0800

    added server health check.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,17 +10,17 @@ v1Router.use("/users", userRouter);
 v1Router.use("/api", apiRouter);
 v1Router.get("/", async (_req: Request, res: Response) => {
 
-    const healthCheck = {
-        uptime: process.uptime(),
-        message: 'OK',
-        timestamp: Date.now()
-    };
-    try {
-        res.status(200).send(healthCheck);
-    } catch (error) {
-        healthCheck.message = error;
-        res.status(503).send();
-    }
+	const healthCheck = {
+		uptime: process.uptime(),
+		message: "OK",
+		timestamp: Date.now()
+	};
+	try {
+		res.status(200).send(healthCheck);
+	} catch (error) {
+		healthCheck.message = error;
+		res.status(503).send();
+	}
 });
 
 export { v1Router };
