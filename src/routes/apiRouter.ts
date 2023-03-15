016892cde1a00d80c11dcbfc8911654099e58d5c
EricSherlockLi <helloleolee@hotmail.com>
2023-03-15 22:20:44 +0800

    1.01

--- src/routes/apiRouter.ts
+++ src/routes/apiRouter.ts
@@ -1,6 +1,6 @@
+import { cancelSubscription } from "../controllers/payment/index";
 import { Router } from "express";
-import { evaluateWriting } from "../controllers/index";
-import { WritingStatistics } from "../controllers/index";
+import { evaluateWriting, WritingStatistics } from "../controllers/index";
 import { tokenGuard } from "../middlewares/index";
 
 const apiRouter = Router();
@@ -8,5 +8,8 @@ const apiRouter = Router();
 // evaluate writing (core function)
 apiRouter.post("/evaluate", evaluateWriting);
 apiRouter.post("/writingStatistics",tokenGuard, WritingStatistics);
+
+// payment
+apiRouter.post("/cancelSubscription", cancelSubscription);
 
 export { apiRouter };
