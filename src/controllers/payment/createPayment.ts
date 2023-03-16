ebb446f40ce68d4fc318808353393ac8ae913c77
Athena Wu <wushuchen113@gmail.com>
2023-03-16 18:24:10 +1030

    change one time payment to subscription

--- src/controllers/payment/createPayment.ts
+++ src/controllers/payment/createPayment.ts
@@ -6,40 +6,19 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 	apiVersion: "2022-11-15",
 });
 
-// const storeItems: Map<number, { priceInCents: number, name: string }> = new Map([
-// 	[1, { priceInCents: 10000, name: "Learn React Today" }],
-// 	[2, { priceInCents: 20000, name: "Learn CSS Today" }],
-// ]);
-// interface Item {
-//   id: string;
-//   quantity: number;
-// }
 
-// interface PaymentRequestBody {
-//   items: Item[];
-// }
-// < PaymentRequestBody>
-const priceInCents = 10000;
 const createPayment = async (req: Request, res: Response) => {
-  
+	
 	try {
-		const { items } = req.body;
-		const { id, quantity } = items[0];
-		console.log(items);
-		console.log(quantity);
+		const { items,planId } = req.body;
 
+		console.log(items);
 		const session = await stripe.checkout.sessions.create({
 			payment_method_types: ["card"],
-			mode: "payment",
+			mode: "subscription",
 			line_items: [
 				{
-					price_data: {
-						currency: "usd",
-						product_data: {
-							name: id,
-						},
-						unit_amount: priceInCents * quantity,
-					},
+					price: planId,
 					quantity: 1,
 				},
 			],
@@ -48,39 +27,8 @@ const createPayment = async (req: Request, res: Response) => {
 		});
 		res.status(200).json({ url: session.url });
 	} catch (error) {
-		console.error(error);
-		res.status(500).send("???");
+		res.status(500).send(error);
 	}
-	// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-	// 	apiVersion: "2022-11-15",
-	// });
-	// try {
-	// 	console.log("hii");
-	// 	console.log(process.env.STRIPE_SECRET_KEY);
-	// 	const session = await stripe.checkout.sessions.create({
-	// 		payment_method_types: ["card"],
-	// 		line_items: [
-	// 			{
-	// 				price_data: {
-	// 					currency: "usd",
-	// 					product_data: {
-	// 						name: "T-shirt",
-	// 					},
-	// 					unit_amount: 9,
-	// 				},
-	// 				quantity: 1,
-	// 			},
-	// 		],
-	// 		mode: "payment",
-	// 		success_url: "http://localhost:3000/user/success",
-	// 		cancel_url: "http://localhost:3000/user/cancel",
-	// 	});
-
-	// 	res.json(session);
-	// } catch (err) {
-	// 	console.error(err);
-	// 	res.status(500).json({ error: "Something went wrong" });
-	// }
 };
 
 export default createPayment;
