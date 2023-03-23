87045c66875516c89d3c145a3bf0b57addac7948
Lucas Lin <dl.world@hotmail.com>
2023-03-23 14:44:14 +1100

    no next step

--- src/controllers/payment/monitorMonthlyPay.ts
+++ src/controllers/payment/monitorMonthlyPay.ts
@@ -7,32 +7,32 @@ import { Request, Response } from "express";
 
 // this middleware has not been tested
 const monitorMonthlyPay = async (req: Request, res: Response) => {
-  const event = req.body;
+	const event = req.body;
 
-  try {
-    const subscriptionId = event.data.object.subscription;
-    const amount = event.data.object.amount_paid;
-    const payTime = event.data.object.paid_at;
-    // "switch" statement to handle different types of Stripe events received on endpoint
-    // checking if "event type" == "invoice.payment_succeeded"
-    switch (event.type) {
-      case "invoice.payment_succeeded":
-        console.log(
-          `Payment of ${amount} received for subscription ${subscriptionId} at ${payTime}`
-        );
-        // break to exit the case
-        // default executed when unrecognised event type is received from Stripe
-        // break + default = function only handles expected events, and alerts unexpected ones
-        break;
-      default:
-        console.log(`Unhandled event type: ${event.type}`);
-    }
+	try {
+		const subscriptionId = event.data.object.subscription;
+		const amount = event.data.object.amount_paid;
+		const payTime = event.data.object.paid_at;
+		// "switch" statement to handle different types of Stripe events received on endpoint
+		// checking if "event type" == "invoice.payment_succeeded"
+		switch (event.type) {
+		case "invoice.payment_succeeded":
+			console.log(
+				`Payment of ${amount} received for subscription ${subscriptionId} at ${payTime}`
+			);
+			// break to exit the case
+			// default executed when unrecognised event type is received from Stripe
+			// break + default = function only handles expected events, and alerts unexpected ones
+			break;
+		default:
+			console.log(`Unhandled event type: ${event.type}`);
+		}
 
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
 };
 
 export { monitorMonthlyPay };
