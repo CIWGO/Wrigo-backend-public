f5af29a1ec405c50c8e3b8ded53a5bf97e9026ea
Lucas Lin <dl.world@hotmail.com>
2023-03-23 15:22:47 +1100

    debugging starts

--- src/controllers/payment/monitorMonthlyPay.ts
+++ src/controllers/payment/monitorMonthlyPay.ts
@@ -1,25 +1,29 @@
 import { Request, Response } from "express";
-// import { Stripe } from "stripe";
+import { Stripe } from "stripe";
 
-// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-//   apiVersion: "2022-11-15",
-// });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-11-15",
+});
 
 // this middleware has not been tested
 const monitorMonthlyPay = async (req: Request, res: Response) => {
-	const event = req.body;
 
 	try {
-		const subscriptionId = event.data.object.subscription;
-		const amount = event.data.object.amount_paid;
-		const payTime = event.data.object.paid_at;
+		const stripeEvent = req.body as Stripe.Event;
+		const subscriptionId = stripeEvent.data.object.subscription as string;
+		const amount = stripeEvent.data.object.amount_paid as number;
+		const payTime = stripeEvent.data.object.paid_at as number;
 		// "switch" statement to handle different types of Stripe events received on endpoint
 		// checking if "event type" == "invoice.payment_succeeded"
-		switch (event.type) {
+		switch (stripeEvent.type) {
 		case "invoice.payment_succeeded":
 			console.log(
 				`Payment of ${amount} received for subscription ${subscriptionId} at ${payTime}`
 			);
+			// break to exit the case
+			break;
+		case "invoice.payment_failed":
+			console.log(`Payment failed for subscription ${subscriptionId} at ${payTime}`)
 			// break to exit the case
 			// default executed when unrecognised event type is received from Stripe
 			// break + default = function only handles expected events, and alerts unexpected ones
