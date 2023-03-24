d8f6d520b508361887b63ee2da2aee9bf568e31c
Lucas Lin <dl.world@hotmail.com>
2023-03-24 19:41:48 +1100

    data required listed in comment

--- src/controllers/payment/monitorMonthlyPay.ts
+++ src/controllers/payment/monitorMonthlyPay.ts
@@ -4,6 +4,11 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2022-11-15",
 });
 
+// require the following data:
+// invoice.paid=true  OR  invoice.paid=false
+// subscription.status=active invoice.status=paid
+// OR
+// subscription.status=incomplete invoice.status=open
 const monitorMonthlyPay = async (req, res) => {
   const event = req.body;
   const succeededInvoice = event.data.object;
@@ -13,12 +18,10 @@ const monitorMonthlyPay = async (req, res) => {
   try {
     switch (event.type) {
       case "invoice.payment_succeeded":
-
         await stripe.subscriptions.retrieve(succeededSubscriptionId);
         // Handle successful payment and active subscription
         break;
       case "invoice.payment_failed":
-
         await stripe.subscriptions.retrieve(failedSubscriptionId);
         // Handle failed payment and incomplete subscription
         break;
