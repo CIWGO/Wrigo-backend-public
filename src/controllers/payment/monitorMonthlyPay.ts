b337327800811095b3ae2f28197fb6be816123fb
Lucas Lin <dl.world@hotmail.com>
2023-03-24 19:37:32 +1100

    Stripe doc ref == billing/subscriptions/overview

--- src/controllers/payment/monitorMonthlyPay.ts
+++ src/controllers/payment/monitorMonthlyPay.ts
@@ -1,55 +1,39 @@
-import { Request, Response } from "express";
 import { Stripe } from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2022-11-15",
 });
 
-// this middleware is to check if payment went through
-const monitorMonthlyPay = async (req: Request, res: Response) => {
-  interface MyWebhookEvent extends Stripe.Event {
-    data: {
-      object: {
-        subscription: string;
-        amount_paid: number;
-        paid_at: number;
-      };
-    };
-  }
-  //  this is to ensure data comes from webHookEvent
-  const stripeEvent = req.body as MyWebhookEvent;
-
+const monitorMonthlyPay = async (req, res) => {
+  const event = req.body;
+  const succeededInvoice = event.data.object;
+  const succeededSubscriptionId = succeededInvoice.subscription;
+  const failedInvoice = event.data.object;
+  const failedSubscriptionId = failedInvoice.subscription;
   try {
-    const subscriptionId = stripeEvent.data.object.subscription;
-    const amount = stripeEvent.data.object.amount_paid;
-    const payTime = stripeEvent.data.object.paid_at;
-    // switch statement to handle different types of events
-    switch (stripeEvent.type) {
+    switch (event.type) {
       case "invoice.payment_succeeded":
-        console.log(
-          `Payment of ${amount} received for subscription ${subscriptionId} at ${payTime}`
-        );
-        // TODO: Send email notification to user about successful payment
-        // break ends current case
+
+        await stripe.subscriptions.retrieve(succeededSubscriptionId);
+        // Handle successful payment and active subscription
         break;
       case "invoice.payment_failed":
-        console.log(
-          `Payment of ${amount} for subscription ${subscriptionId} failed at ${payTime}`
-        );
-        // TODO: Send email notification to user about payment failure
-        // break ends current case
+
+        await stripe.subscriptions.retrieve(failedSubscriptionId);
+        // Handle failed payment and incomplete subscription
         break;
-      // default is executed in when no case matches the value of the expression
+      case "customer.subscription.updated":
+        break;
       default:
-        console.log(`Unhandled event type: ${stripeEvent.type}`);
+        // Unexpected event type
+        break;
     }
+
+    // res.status(200).send("OK");
   } catch (error) {
-    console.log(error);
-    res.status(400).send(`Webhook Error: ${error.message}`);
+    console.error(error);
+    res.status(500).send("Error");
   }
 };
-
-// reference
-// https://stripe.com/docs/payments/payment-intents/verifying-status
 
 export { monitorMonthlyPay };
