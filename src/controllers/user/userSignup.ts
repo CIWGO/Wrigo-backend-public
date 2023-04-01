d817697668183397d63ac2e261847fb4edf1f14f
leo <leoyh.97@gmail.com>
2023-04-01 19:48:37 +0800

    CIWGO-219-Respond-token-in-signup

--- src/controllers/user/userSignup.ts
+++ src/controllers/user/userSignup.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
 import { createOperationLog } from "../log/index";
 import { createUserProfile } from "../index";
+import { generateToken } from "../../utils/jwt";
 
 /**
  * Create new user from signup.
@@ -31,6 +32,17 @@ const createUser = async (req: Request, res: Response) => {
 			await user.save();
 			//create new userProfile
 			await createUserProfile(uid, username);
+
+			const payload = {
+				username: user.username,
+				uid: user.uid,
+				email: user.email,
+			};
+			
+			const token = generateToken(payload);
+	
+			const userlog={token:token,username:username,uid:user.uid};
+
 			// create operation log and store it to DB
 			createOperationLog(
 				false,
@@ -40,7 +52,7 @@ const createUser = async (req: Request, res: Response) => {
 				req.userDevice,
 				uid
 			);
-			return res.status(201).json({ username, email });
+			return res.status(201).json({ message: "Sign up successful", ...userlog });
 		} else {
 			// create operation log and store it to DB
 			createOperationLog(
