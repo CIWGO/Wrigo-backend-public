c7e33148eda95db1b3766fdba0876aec259d5cf8
Athena Wu <wushuchen113@gmail.com>
2023-04-03 16:26:00 +0930

    add addSubscribedSince function

---
+++ src/controllers/payment/addSubscribedSince.ts
@@ -0,0 +1,22 @@
+import UserAccount from "../../models/user/userAccount";
+import  PaymentHistory  from "../../models/payment/paymentHistory";
+
+const newToSubscription = async (uid) => { 
+  const user = await UserAccount.findOne({ uid });
+  try {
+    if (!user.isSubscribed) {
+      // If the user is a new user, add the current date to the payment collection
+      const subscriptionSince = new Date();
+      await PaymentHistory.findOneAndUpdate(
+        { uid },
+        { $set: { subscriptionSince } },
+        { new: true }
+      );
+    }
+  } catch (err) { 
+    console.log("error in set current date to PaymentHistory", err);
+  }
+  
+};
+
+export default newToSubscription;
--- src/controllers/payment/webhook.ts
+++ src/controllers/payment/webhook.ts
@@ -5,6 +5,7 @@ import { createPaymentInvoice } from "../../utils/db/createPaymentInvoice";
 import createOrUpdatePaymentHistory from "../../utils/db/createOrUpdatePaymentHistory";
 import { userAccount as UserModel } from "../../models/index";
 import { cancelSubscriptionImmediately } from "./cancelSubscriptionImmediately";
+import newToSubscription from "./addSubscribedSince";
 
 const STRIPE_SECRET_KEY = config.STRIPE_SECRET_KEY;
 const STRIPE_WEBHOOK_SECRET = config.STRIPE_WEBHOOK_SECRET;
@@ -60,6 +61,7 @@ const handleStripeWebhook = async (req: Request, res: Response) => {
 
 		await createOrUpdatePaymentHistory(uid, customerId, subscriptionId, latestInvoice);
 
+		await newToSubscription(uid);
 		await UserModel.findOneAndUpdate(
 			{ uid },
 			{
@@ -103,86 +105,3 @@ const handleStripeWebhook = async (req: Request, res: Response) => {
 };
 
 export default handleStripeWebhook;
-
-// import { Request, Response } from "express";
-// import Stripe from "stripe";
-// import userPaymentHistory from "../../models/payment/paymentHistory";
-// import { sendEmail } from "../../utils/ses_sendEmail";
-// import findEmailByUid from "../../utils/db/findEmailByUid";
-// import config from "../../../config";
-
-// const STRIPE_SECRET_KEY = config.STRIPE_SECRET_KEY;
-// const STRIPE_WEBHOOK_SECRET = config.STRIPE_WEBHOOK_SECRET;
-
-// const stripe = new Stripe(STRIPE_SECRET_KEY, {
-// 	apiVersion: "2022-11-15",
-// });
-
-// export const handleStripeWebhook = async (req: Request, res: Response) => {
-// 	console.log("*** handleStripeWebhook start");
-
-// 	const sig = req.headers["stripe-signature"];
-// 	const endpointSecret = STRIPE_WEBHOOK_SECRET;
-
-// 	let event: Stripe.Event;
-
-// 	try {
-// 		event = stripe.webhooks.constructEvent(req.body.toString(), sig, endpointSecret);
-// 	} catch (err) {
-// 		console.error(err.message);
-// 		return res.status(400).send(`Webhook Error: ${err.message}`);
-// 	}
-
-// 	const invoice = event.data.object as Stripe.Invoice;
-// 	const customer = await stripe.customers.retrieve(invoice.customer.toString()) as Stripe.Customer;
-// 	const uid = customer.metadata.uid;
-
-// 	console.log("webhook uid", uid);
-
-// 	await updatePaymentHistory(uid, invoice);
-// 	const userEmail = await findEmailByUid(uid);
-
-
-// 	// Handle the event based on its type
-// 	switch (event.type) {
-// 		case "invoice.payment_succeeded":
-// 			console.log(`Payment succeeded for invoice ${invoice.id}`);
-// 			// send email to user
-// 			await sendEmail(
-// 				[userEmail],
-// 				"WRIGO - subscription receipt",
-// 				"Monthly payment success."
-// 			);
-// 			break;
-// 		case "invoice.payment_failed":
-// 			console.log(`Payment failed for invoice ${invoice.id}`);
-// 			await sendEmail(
-// 				[userEmail],
-// 				"WRIGO - payment failed",
-// 				"Monthly payment failed."
-// 			);
-// 			// here to call the cancel subscription api
-// 			break;
-// 		default:
-// 			console.log(`Unhandled event type ${event.type}`);
-// 	}
-
-// 	res.json({ received: true });
-// };
-
-
-// const updatePaymentHistory = async (uid, latestInvoice) => {
-// 	await userPaymentHistory.findOneAndUpdate(
-// 		{ uid },
-// 		{
-// 			$push: {
-// 				invoices: latestInvoice,
-// 			},
-// 		},
-// 		{
-// 			upsert: true, // Create a new document if it doesn't exist
-// 		}
-// 	);
-// };
-
-// export default handleStripeWebhook;
