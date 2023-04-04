9f70411663b9a6b6f45cc73640b6e2dbfba66d67
Chang Liu <changstudy19@gmail.com>
2023-04-04 17:15:12 +0930

    fix conflict

--- src/controllers/payment/webhook.ts
+++ src/controllers/payment/webhook.ts
@@ -1,14 +1,10 @@
-
- 
-
 import { Request, Response } from "express";
 import Stripe from "stripe";
 import config from "../../../config";
 import { createPaymentInvoice } from "../../utils/db/createPaymentInvoice";
 import createOrUpdatePaymentHistory from "../../utils/db/createOrUpdatePaymentHistory";
-import { userAccount as UserModel } from "../../models/index";
-import { cancelSubscriptionImmediately }  from "./cancelSubscriptionImmediately";
 import addSubscribedSince from "./addSubscribedSince";
+import { changeIsSubscribed } from "../../utils/db/changeIsSubscribed";
 
 const STRIPE_SECRET_KEY = config.STRIPE_SECRET_KEY;
 const STRIPE_WEBHOOK_SECRET = config.STRIPE_WEBHOOK_SECRET;
@@ -62,17 +58,10 @@ const handleStripeWebhook = async (req: Request, res: Response) => {
 		console.log("subscriptionId", subscriptionId);
 		// console.log("latestInvoice", latestInvoice);
 
-		await createOrUpdatePaymentHistory(uid, customerId, subscriptionId,paymentMethod, latestInvoice);
+		await createOrUpdatePaymentHistory(uid, customerId, subscriptionId, paymentMethod, latestInvoice);
 
 		await addSubscribedSince(uid);
-		await UserModel.findOneAndUpdate(
-			{ uid },
-			{
-				$set: {
-					isSubscribed: true
-				}
-			},
-		);
+		await changeIsSubscribed(uid, true);
 		return res.status(200).send();// send 200 to stripe
 
 		// stripe is expected to send automated email to real subscribers
@@ -95,15 +84,26 @@ const handleStripeWebhook = async (req: Request, res: Response) => {
 		const status = invoice.status;// get invoice status
 
 		const uid = customer.metadata.uid;// get uid
-		const subscriptionId = invoice.subscription;// get subscription id
+		// const subscriptionId = invoice.subscription;// get subscription id
 
 		const latestInvoice = await createPaymentInvoice(invoiceId, createdDate, paymentMethod, paymentAmount, status);// create latest invoice
 		await latestInvoice.save();// save invoice into invoice collection
 
-		await cancelSubscriptionImmediately(uid, subscriptionId, latestInvoice);
+		await createOrUpdatePaymentHistory(uid, null, null, latestInvoice);// add failure invoice into payment history, change customerId and subscriptionId to null
+		// await cancelSubscriptionImmediately(uid, subscriptionId, latestInvoice);
 		return res.status(200).send();// send 200 to stripe
 
 		// stripe is expected to send automated email to real subscribers
+	} else if (event.type === "customer.subscription.deleted") {
+		const subscription = event.data.object as Stripe.Subscription;
+		const customerId = subscription.customer as string;
+		// Retrieve the customer object
+		const customer = await stripe.customers.retrieve(customerId) as Stripe.Customer;
+		// Get uid from metadata
+		const uid = customer.metadata.uid;
+		await changeIsSubscribed(uid, false);// set isSubscribed to false
+
+		return res.status(200).send();// send 200 to stripe
 	}
 };
 
