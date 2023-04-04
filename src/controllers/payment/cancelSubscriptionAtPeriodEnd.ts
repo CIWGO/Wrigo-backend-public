a31e24b554bca453e6e49289b8fd3f9dbfbc7288
Chang Liu <changstudy19@gmail.com>
2023-04-04 17:36:42 +0930

    fix cancelSubscriptionAtPeriodEnd

--- src/controllers/payment/cancelSubscriptionAtPeriodEnd.ts
+++ src/controllers/payment/cancelSubscriptionAtPeriodEnd.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from "express";
 import getSubscriptionIdByUid from "../../utils/db/getSubscriptionIdByUid";
 import Stripe from "stripe";
 
@@ -5,13 +6,15 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 	apiVersion: "2022-11-15",
 });
 
-const cancelSubscriptionAtPeriodEnd = async (uid) => {
+const cancelSubscriptionAtPeriodEnd = async (req: Request, res: Response) => {
+	const { uid } = req.body;
 	const subscriptionId = await getSubscriptionIdByUid(uid);
 	if (subscriptionId) {
 		try {
 			await stripe.subscriptions.update(subscriptionId, {
 				cancel_at_period_end: true,
 			});
+			res.status(200).json("subscription will be cancelled at period end");
 		} catch (error) {
 			console.log("Error updating subscription:", error);
 		}
